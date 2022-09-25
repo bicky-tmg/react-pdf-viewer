c4775522d72c6f7fd0a157ba27c547ec7b924a95
Bicky Tamang <bickytamang101@gmail.com>
2022-09-25 08:40:00 +0545

    basic setup and config for react pdf viewer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+// Import the styles provided by the react-pdf-viewer packages
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
